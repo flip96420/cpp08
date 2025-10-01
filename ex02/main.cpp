6b3bb8de2aac69c45966fa59b38d093fecb02cbb
Philipp Schmunk <pschmunk@c2r7s4.42wolfsburg.de>
2025-10-01 16:18:56 +0200

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pschmunk <pschmunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 18:04:23 by pschmunk          #+#    #+#             */
-/*   Updated: 2025/10/01 16:01:39 by pschmunk         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:16:16 by pschmunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,33 @@
 
 int main(void)
 {
+	// Int stack
 	MutantStack<int> int_stack;
-	MutantStack<int>::rev_iterator int_it = int_stack.rbegin();
-
 	int_stack.push(1);
 	int_stack.push(2);
 	int_stack.push(3);
-	while (int_it != int_stack.rend())
+	
+	MutantStack<int>::iterator int_it = int_stack.begin();
+
+	while (int_it != int_stack.end())
 	{
-		std::cout	<< "TEST: " << *int_it << std::endl;
+		std::cout	<< "Int stack: " << *int_it << std::endl;
 		int_it++;
 	}
 
+	// String stack
+	MutantStack<std::string> str_stack;
+	str_stack.push("drei");
+	str_stack.push("zwei");
+	str_stack.push("eins");
 
-	// MutantStack<std::string> str_stack;
-	// MutantStack<std::string>::iterator str_it = str_stack.rbegin();
+	MutantStack<std::string>::rev_iterator str_it = str_stack.rbegin();
 
-	// str_stack.push("drei");
-	// str_stack.push("zwei");
-	// str_stack.push("eins");
-	// while (str_it != str_stack.rend())
-	// {
-	// 	std::cout	<< *str_it << std::endl;
-	// 	str_it++;
-	// }
+	while (str_it != str_stack.rend())
+	{
+		std::cout	<< "String stack: " << *str_it << std::endl;
+		str_it++;
+	}
 	
 	return (0);
 }
