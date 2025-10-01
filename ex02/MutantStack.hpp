a79b7dae3f95bcf6263df6abce7c3a8742c1e8de
Philipp Schmunk <pschmunk@c2r7s4.42wolfsburg.de>
2025-10-01 16:04:55 +0200

    almost done with ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: pschmunk <pschmunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 17:36:51 by pschmunk          #+#    #+#             */
-/*   Updated: 2025/09/24 17:40:27 by pschmunk         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:52:01 by pschmunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,65 @@
 #include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
-private:
-	T	*stack;
-public:
-	MutantStack()
-	{
-		
-	};
-	~MutantStack();
+	public:
+		typedef std::stack<T> stack;
+		typedef typename stack::container_type::iterator iterator;
+		typedef typename stack::container_type::const_iterator const_iterator;
+		typedef typename stack::container_type::reverse_iterator rev_iterator;
+		typedef typename stack::container_type::const_reverse_iterator const_rev_iterator;
+
+		MutantStack() : stack(){};
+		MutantStack(const MutantStack &copy)
+		{
+			*this = copy;
+		};
+		MutantStack	&operator=(const MutantStack &copy)
+		{
+			if (copy != *this)
+				this->name = copy.name;
+			return (*this);
+		};
+		~MutantStack(){};
+
+		// iterator
+		iterator begin()
+		{
+			return (stack::c.begin());
+		}
+		iterator end()
+		{
+			return (stack::c.end());
+		}
+
+		// constant iterator
+		const_iterator begin() const
+		{
+			return (stack::c.begin());
+		}
+		const_iterator end() const
+		{
+			return (stack::c.end());
+		}
+
+		// reverse iterator
+		rev_iterator rbegin()
+		{
+			return (stack::c.rbegin());
+		}
+		rev_iterator rend()
+		{
+			return (stack::c.rend());
+		}
+
+		// constant reverse iterator
+		const_rev_iterator rbegin() const
+		{
+			return (stack::c.rbegin());
+		}
+		const_rev_iterator rend() const
+		{
+			return (stack::c.rend());
+		}
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pschmunk <pschmunk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 18:04:23 by pschmunk          #+#    #+#             */
-/*   Updated: 2025/09/24 18:23:56 by pschmunk         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:01:39 by pschmunk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,30 @@
 
 int main(void)
 {
+	MutantStack<int> int_stack;
+	MutantStack<int>::rev_iterator int_it = int_stack.rbegin();
 
+	int_stack.push(1);
+	int_stack.push(2);
+	int_stack.push(3);
+	while (int_it != int_stack.rend())
+	{
+		std::cout	<< "TEST: " << *int_it << std::endl;
+		int_it++;
+	}
+
+
+	// MutantStack<std::string> str_stack;
+	// MutantStack<std::string>::iterator str_it = str_stack.rbegin();
+
+	// str_stack.push("drei");
+	// str_stack.push("zwei");
+	// str_stack.push("eins");
+	// while (str_it != str_stack.rend())
+	// {
+	// 	std::cout	<< *str_it << std::endl;
+	// 	str_it++;
+	// }
+	
 	return (0);
 }
